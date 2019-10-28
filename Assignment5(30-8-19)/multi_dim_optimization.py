6b071b3c8b0896c3ecf8fb2e51d4526261130075
Himanshu Kumar <31369145+trunkboy@users.noreply.github.com>
2019-10-28 12:54:11 +0530

    Add files via upload

---
+++ Assignment5(30-8-19)/multi_dim_optimization.py
@@ -0,0 +1,6 @@
+#Problem statement :
+# The cross-sectional area of a gutter is (b + lcos(theta))*lsin(theta)
+# Assume that the width of the material to be bent into the gutter shape is 6 units.
+# FInd the andke theta and the edge length l, which maximizes the cross-sectional area of the gutter with the constratin:
+#  b= 6-2l
+# Consider the initial interval of theta from 0 to pi/2 and l from 0 to 3.  
